ac448819d703423004dedce484ccf1c54f8531b3
Ricardo Soto Estévez <uo265710@uniovi.es>
2020-03-09 19:14:43 +0100

    Fixed buttons from userlist and moved delete code to a js
    Fix #33

--- src/main/resources/static/script/user/list.js
+++ src/main/resources/static/script/user/list.js
@@ -1,3 +1,9 @@
-$( ".addButton" ).click(function() {
-	alert("Solicitud enviada correctamente");
+$("#deleteButton").click(function() {
+	var urlDelete = '/user/list/delete' + "?ids=";
+	var $boxes = $('input[name=cb]:checked');
+	$boxes.each(function() {
+		urlDelete += $(this).attr("id") + ",";
+	});
+	urlDelete = urlDelete.substring(0, urlDelete.length - 1);
+	$("#tableUsers").load(urlDelete);
 });
